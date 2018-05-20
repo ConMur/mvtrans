891a149228804acdcd33030714f34ea11bb38c50
Connor <connor.murphy@bell.net>
2018-05-20 19:13:44 -0400

    Temp main changes until CLI finalized

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,25 @@ use std::env;
 use std::path::Path;
 
 use mvtrans::parser::Parser;
+use mvtrans::patcher::Patcher;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let filename = &args[1];
-
-    let path = Path::new(filename);
-
-    let parser : Parser = Parser::new(path);
+    let source_file = format!("{}.json", &args[1]);
+    let patch_file = format!("{}.txt", &args[1]);
+/*
+    let parser : Parser = Parser::new(&source_file);
 
     let lines = parser.parse();
 
-    mvtrans::parser::write_to_file(&parser, lines);
+    mvtrans::parser::write_to_file(&parser, lines);*/
+    
+
+    let mut patcher : Patcher = Patcher::new(source_file, patch_file);
+
+    patcher.patch();
+
+    patcher.write_to_file();
    
 }
